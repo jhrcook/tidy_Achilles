fa47f388a0793368a1d4bd2f338553074f2c89c4
jhrcook <joshuacook0023@gmail.com>
2019-06-11 13:45:13 -0400

    fix: use janitor::clean_names() and fix some of my out-dated idioms

--- data_preparation.R
+++ data_preparation.R
@@ -16,54 +16,37 @@ save_tib_by_tissue <- function(tissue, data, save_dir, save_suffix) {
     cat("working on", tissue, "\n")
     new_data <- data %>%
         mutate(tissue = !!tissue)
-    save_name <- file.path(data_dir, save_dir,
-                           paste0(tissue, save_suffix))
+    save_name <- file.path(
+        data_dir, save_dir,
+        paste0(tissue, save_suffix)
+    )
     saveRDS(new_data, save_name)
 }
 
 
 #### ---- Cell Line Meta ---- ####
 ccle <- read_csv(data_file_path("DepMap-2019q1-celllines_v2.csv")) %>%
-    jhcutils::tidy_colnames() %T>%
+    janitor::clean_names() %T>%
     saveRDS(data_file_path("cell_line_metadata.tib"))
 
 
 #### ---- Cell Line Mutations ---- ####
-cell_mutations <- read_csv(data_file_path("depmap_19Q1_mutation_calls.csv")) %>%
-    dplyr::select(-X1) %T>%
+read_csv(data_file_path("depmap_19Q1_mutation_calls.csv")) %>%
+    janitor::clean_names() %>%
+    dplyr::select(-x1) %>%
     saveRDS(data_file_path("cell_line_mutations.tib"))
-
-
-#### ---- Ras-mutant Cell Lines (with anno.) ---- ####
-# TODO: remove
-# ras_muts <- cell_mutations %>%
-#     filter(Hugo_Symbol %in% c("KRAS", "NRAS", "HRAS")) %>%
-#     mutate(amino_acid_mut = str_remove_all(Protein_Change, "^p\\.")) %>%
-#     dplyr::select(Tumor_Sample_Barcode, Hugo_Symbol, amino_acid_mut) %>%
-#     mutate(ras_allele = paste0(Hugo_Symbol, "_", amino_acid_mut))
-# colnames(ras_muts) <- c("DepMap_ID", "ras", "allele", "ras_allele")
-# ras_muts_anno <- left_join(ras_muts, ccle, by = "DepMap_ID") %T>%
-#     saveRDS(data_file_path("ras_muts_annotated.tib"))
-
-
-#### ---- Cell Line Meta (with Ras-mutant anno.) ---- ####
-# TODO: remove
-# ccle_ras_muts <- full_join(ccle, ras_muts, by = "DepMap_ID") %>%
-#     mutate(ras = ifelse(is.na(ras), "WT", ras),
-#            allele = ifelse(is.na(allele), "WT", allele),
-#            ras_allele = ifelse(is.na(ras_allele), "WT", ras_allele)) %T>%
-#     saveRDS(data_file_path("cell_line_ras_anno.tib"))
 
 
 #### ---- RNAi Synthetic Lethal Data ---- ####
 rnai_synlet <- read_csv(data_file_path("D2_combined_gene_dep_scores.csv")) %>%
-    gather(cell_line, score, `127399_SOFT_TISSUE`:`ZR75B_BREAST`) %>%
-    dplyr::rename(gene = "X1") %>%
-    mutate(Entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
+    janitor::clean_names() %>%
+    gather(key = "cell_line", value = "achilles_score", -x1) %>%
+    dplyr::rename(gene = "x1") %>%
+    mutate(entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
            gene = str_extract(gene, "^.+(?=[:space:])"),
            tissue = str_split_fixed(cell_line, "_", 2)[, 2]) %>%
-    select(gene, Entrez, cell_line, tissue, score) %>%
-    left_join(ccle, by = c("cell_line" = "CCLE_Name")) %T>%
+    select(gene, entrez, cell_line, tissue, achilles_score) %>%
+    left_join(ccle, by = c("cell_line" = "ccle_name")) %T>%
     saveRDS(data_file_path("rnai_synthetic_lethal.tib"))
 
 a <- rnai_synlet %>%
@@ -73,48 +56,56 @@ a <- rnai_synlet %>%
          save_dir = "rnai_synthetic_lethal",
          save_suffix = "_rnai_synlet.tib")
 
+rm(rnai_synlet)
+
 
 #### ---- Gene Effect ---- ####
-gene_effect <- read_csv(data_file_path("gene_effect_corrected.csv")) %>%
-    dplyr::rename(DepMap_ID = "X1") %>%
-    tidyr::gather(key = "gene", value = "CERES_score", -DepMap_ID) %>%
-    mutate(Entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
+read_csv(data_file_path("gene_effect_corrected.csv")) %>%
+    janitor::clean_names() %>%
+    dplyr::rename(dep_map_id = "x1") %>%
+    tidyr::gather(key = "gene", value = "ceres_score", -dep_map_id) %>%
+    mutate(entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
            gene = str_extract(gene, "^.+(?=[:space:])")) %>%
-    left_join(ccle, by = "DepMap_ID") %T>%
+    left_join(ccle, by = "dep_map_id") %>%
     saveRDS(data_file_path("gene_effect.tib"))
 
 
 #### ---- Gene Dependency ---- ####
-gene_depend <- read_csv(data_file_path("gene_dependency_corrected.csv")) %>%
-    dplyr::rename(DepMap_ID = "line") %>%
-    tidyr::gather(key = "gene", value = "dependency_score", -DepMap_ID) %>%
-    mutate(Entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
+read_csv(data_file_path("gene_dependency_corrected.csv")) %>%
+    janitor::clean_names() %>%
+    dplyr::rename(dep_map_id = "line") %>%
+    tidyr::gather(key = "gene", value = "dependency_score", -dep_map_id) %>%
+    mutate(entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
            gene = str_extract(gene, "^.+(?=[:space:])")) %>%
-    left_join(ccle, by = "DepMap_ID") %T>%
+    left_join(ccle, by = "dep_map_id") %>%
     saveRDS(data_file_path("gene_dependency.tib"))
 
 
 #### ---- Gene Essentiality  ---- ####
 essential_genes <- read_tsv(data_file_path("essential_genes.txt")) %>%
+    janitor::clean_names() %>%
     mutate(is_essential = TRUE)
 nonessential_genes <- read_tsv(data_file_path("nonessential_genes.txt")) %>%
     mutate(is_essential = FALSE)
-gene_essentiality <- bind_rows(essential_genes, nonessential_genes) %>%
-    mutate(gene = str_extract(gene, "^.+(?=[:space:])")) %T>%
+bind_rows(essential_genes, nonessential_genes) %>%
+    mutate(gene = str_extract(gene, "^.+(?=[:space:])")) %>%
     saveRDS(data_file_path("gene_essentiality.tib"))
+
+rm(essential_genes, nonessential_genes)
 
 
 #### ---- CNV ---- ####
 cn_tib <- read_csv(data_file_path("public_19Q1_gene_cn.csv"),
                     col_types = cols(.default = "c")) %>%
-    dplyr::rename(DepMap_ID = "X1") %>%
-    tidyr::gather(key = "gene", value = "copy_number", -DepMap_ID) %>%
+    janitor::clean_names() %>%
+    dplyr::rename(dep_map_id = "x1") %>%
+    tidyr::gather(key = "gene", value = "copy_number", -dep_map_id) %>%
     mutate(copy_number = as.numeric(copy_number),
            gene = str_extract(gene, "^.+(?=[:space:])"))  %>%
-    left_join(ccle, by = "DepMap_ID")  %>%
-    mutate(tissue = str_remove_all(CCLE_Name, "\\[MERGED_TO.+\\]")) %>%
+    left_join(ccle, by = "dep_map_id")  %>%
+    mutate(tissue = str_remove_all(ccle_name, "\\[MERGED_TO.+\\]")) %>%
     mutate(tissue = str_split_fixed(tissue, "_", 2)[, 2]) %>%
-    mutate(tissue = ifelse(is.na(CCLE_Name), "UNKNOWN", tissue)) %T>%
+    mutate(tissue = ifelse(is.na(ccle_name), "UNKNOWN", tissue)) %T>%
     saveRDS(data_file_path("cell_line_copy_number.tib"))
 
 a <- cn_tib %>%
@@ -124,9 +115,12 @@ a <- cn_tib %>%
          save_dir = "copy_number",
          save_suffix = "_copynum.tib")
 
+rm(cn_tib)
+
 
 #### ---- Guide Maps ---- ####
-guide_tib <- read_csv("data/guide_gene_map.csv") %>%
-    mutate(Entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
-           gene = str_extract(gene, "^.+(?=[:space:])")) %T>%
+read_csv("data/guide_gene_map.csv") %>%
+    janitor::clean_names() %>%
+    mutate(entrez = str_extract(gene, "(?<=\\()[:digit:]+(?=\\))"),
+           gene = str_extract(gene, "^.+(?=[:space:])")) %>%
     saveRDS(file.path("data", "guide_gene_map.tib"))
